b8485ca41d6af534b0039ffb608f0a6407520230
alekseiishevchuk <shevchuka@gmail.com>
2017-01-10 23:25:42 +0200

    refactor Answer

--- app/Answer.php
+++ app/Answer.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AbstractAnswer extends Model
+class Answer extends Model
 {
     public $timestamps = false;
     protected $table = 'answers';
--- app/AnswerText.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-
-class AnswerText extends AbstractAnswer
-{
-    //
-}
